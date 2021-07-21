05da438cfcd4bc16d82b4525421f0d5a016dfaf3
fetrajulio <fetrajulio@gmail.com>
2021-07-21 23:39:05 +0200

    commit git2

---
+++ src/Apl2.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class Apl2 extends React.Component{
+    render(){
+        return <h2>Mety</h2>
+    }
+}
+const rootElement = document.getElementById("root2");
+ReactDOM.render(<Apl2 />,rootElement);
+export default Apl2;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Apl2 from './Apl2';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Apl extends React.Component{
+  state ={
+    clients: [
+      {id: 1, nom: "Julio"},
+      {id: 2,nom:"Hazard"},
+      {id: 3,nom:"Halland"}
+    ]
+  }
+  
+  
+  render(){
+    const ttl="Liste De Clients"
+    const element = <li>Kimmich</li>
+    const elements = this.state.clients.map((client)=><li>{client.nom} <button>X</button></li>)
+    return <div>
+      <h1>{ttl}</h1>
+      <ul>
+        {element}
+        {elements}
+      </ul>
+    </div>
+  }
+}
 
+
+
+const rootElement2 = document.getElementById("root");
+ReactDOM.render(<Apl />,rootElement2);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
